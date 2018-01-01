79278b313e756211bcb109d48d95826c101d3601
Paolo Achdjian <pachdjian@gmail.com>
2018-01-01 20:03:29 +0100

    fix device info request

--- Source/SerialReceive/SendReqDeviceInfo.c
+++ Source/SerialReceive/SendReqDeviceInfo.c
@@ -4,7 +4,7 @@
 // 1 byte  -> code
 // 2 bytes -> network id
 void sendReqDeviceInfo(uint8 * data) {
-	uint16 nwkAddr = BUILD_UINT16(data[0], data[1]);
+	uint16 nwkAddr = BUILD_UINT16(data[1], data[2]);
 	
 	associated_devices_t * device= AssocGetWithShort( nwkAddr);
 	if (device == NULL){
